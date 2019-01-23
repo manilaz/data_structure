a4dd419871af475ecd942bde25ddf55062f481e9
manilaz <24566370@qq.com>
2019-01-23 10:19:04 +0800

    快速排序

--- sort/src/main/java/com/zbest/QuickSort.java
+++ sort/src/main/java/com/zbest/QuickSort.java
@@ -7,58 +7,46 @@ import java.util.Arrays;
  */
 public class QuickSort {
 
-
-
     //单次分组
     public static int sort(int[] arr,int start,int end){
 
-        System.out.println("start:"+start+",end:"+end);
         int count = 0;
         if(end <= start) return count;
         int i = start + 1;
         int j = end;
         int tmp ;
         do{
-            System.out.println(arr[i] + "&&" + arr[j]);
-            if (arr[i] > arr[j]) {
+            if (arr[start] > arr[j]) {
                 tmp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = tmp;
                 i++;
-            } else {
+            }else {
                 j--;
             }
             count++;
             if(j<start || i>end) return count;
-
         }while (i != j);
         count ++;
-        System.out.println(arr[start] + "&&&" + arr[j]+"&&&"+arr[i]);
         if(arr[start] > arr[j]){
             tmp = arr[start];
             arr[start] = arr[j];
             arr[j] = tmp;
         }
-
-//        if(start < (end-start)/2)
-//            count += sort(arr,start,(end-start)/2);
-//        if((end-start)/2+1 < end)
-//            count += sort(arr,(end-start)/2+1,end);
-
-        System.out.println(array2String(arr,start,i)+"|"+array2String(arr,i+1,end));
-
-        System.out.println(start+"|"+i+"|"+(i+1)+"|"+end);
-        System.out.println("---------");
+        if(start < j)
+            count += sort(arr,start,j);
+        if(j < end)
+            count += sort(arr,j+1,end);
         return count;
     }
 
-
     public static int sort(int[] arr){
-
         return sort(arr,0,arr.length-1);
     }
 
-
+    public static void print(int[] arr){
+        System.out.println("arr比较次数"+sort(arr)+",排序结果:"+array2String(arr));
+    }
 
     public static String array2String(int[] arr,int start,int end){
         StringBuilder builder = new StringBuilder("[");
@@ -75,14 +63,9 @@ public class QuickSort {
         });
         return builder.append("]").toString().replace(",]","]");
     }
-
-
     public static void main(String[] args) {
         int[] arr = {18,6,47,13,7,32,22,19,5};
-
-        sort(arr,0,8);
-        sort(arr,0,4);
-//        sort(arr);
+        print(arr);
     }
 
 }
